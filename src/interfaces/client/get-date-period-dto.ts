5c199a07d829cd7e992efaf78aff61c33f89c686
Sergei Kurpach <kurpachsv@gmail.com>
2021-12-23 09:07:09 +0300

    style(визард): GBO-19505 исправления для линтинга

--- src/interfaces/client/get-date-period-dto.ts
+++ src/interfaces/client/get-date-period-dto.ts
@@ -1,7 +1,7 @@
 import type { DATE_PERIODS } from 'interfaces';
 
 /** Допустимые значения типа периода для запроса актуальных дат. */
-export type RequestPeriodType =  Exclude<DATE_PERIODS, DATE_PERIODS.SELECT_PERIOD>;
+export type RequestPeriodType = Exclude<DATE_PERIODS, DATE_PERIODS.SELECT_PERIOD>;
 
 /** ДТО запроса периода. */
 export interface IGetDatePeriodRequestDto {
