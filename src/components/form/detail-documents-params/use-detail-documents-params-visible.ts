42f65987d27c874badbe723549fccb181db0cb6f
Sergei Kurpach <kurpachsv@gmail.com>
2022-03-23 13:47:31 +0300

    fix(визард): GBO-23104 исправление условий видимости блока параметров

--- src/components/form/detail-documents-params/use-detail-documents-params-visible.ts
+++ src/components/form/detail-documents-params/use-detail-documents-params-visible.ts
@@ -1,10 +1,14 @@
+import { useContext } from 'react';
 import { CREATION_PARAMS } from 'interfaces/form/creation-params';
+import type { IFormContext } from 'interfaces/form/form-context';
+import { FormContext } from 'interfaces/form/form-context';
 import type { IFormState } from 'interfaces/form/form-state';
 import { useFormState } from 'react-final-form';
 
 /** Хук проверки видимости компонента "Детальные параметры комплекта документов". */
 export const useDetailDocumentsParamsVisible = (): boolean => {
   const { values } = useFormState<IFormState>();
+  const { isPdf } = useContext<IFormContext>(FormContext);
 
-  return values.creationParams.includes(CREATION_PARAMS.WITH_DOCUMENTS_SET);
+  return values.creationParams.includes(CREATION_PARAMS.WITH_DOCUMENTS_SET) && isPdf;
 };
