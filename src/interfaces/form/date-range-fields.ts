92208555a6bef35df1e59f35b2faaca2368db012
Vladimir Mitrokhin <v.mitrokhin@bssys.com>
2022-03-30 14:23:42 +0300

    review: GBO-22118 добавлен js-doc

--- src/interfaces/form/date-range-fields.ts
+++ src/interfaces/form/date-range-fields.ts
@@ -2,6 +2,8 @@
  * Описание полей диапазона дат.
  */
 export interface IDateRangeFields {
+  /** Дата начала периода. */
   dateFrom: string;
+  /** Дата окончания периода. */
   dateTo: string;
 }
