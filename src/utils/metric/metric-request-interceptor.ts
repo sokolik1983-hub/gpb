3170844d9c282f2cd7846be582fc591a1c421f17
Sergei Kurpach <kurpachsv@gmail.com>
2022-06-10 12:30:39 +0300

    fix(метрика): GBO-27621 доработки условий отправки данных в метрику

--- src/utils/metric/metric-request-interceptor.ts
+++ src/utils/metric/metric-request-interceptor.ts
@@ -6,8 +6,6 @@ import { COMMON_STREAM_URL } from 'stream-constants/client';
 
 /** Шаблон регулярного выражения для guid. */
 const guidRegEx = '[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}';
-/** Шаблон регулярного выражения для существующего запроса выписки. */
-const concreteStatementRequestRegEx = new RegExp(`/request/${guidRegEx}$`);
 /** Шаблон регулярного выражения для экспорта выписки. */
 const exportStatementRegEx = new RegExp(`/export/${guidRegEx}$`);
 /** Шаблон регулярного выражения для печати выписки. */
@@ -18,14 +16,14 @@ const exportAttachment = new RegExp(`/attachment/${guidRegEx}$`);
 /** Функция для преобразования url в тип произошедшего действия для метрики. */
 export const mapUrlToMetricAction = (url: string, sourcePage?: string): METRIC_ACTION | undefined => {
   switch (true) {
-    case url.endsWith('/statement'):
-      return METRIC_ACTION.STATEMENT_REQUEST;
-    case (concreteStatementRequestRegEx.test(url) && sourcePage === COMMON_STREAM_URL.STATEMENT_TURNOVER) || url.endsWith('/request'):
+    case url.endsWith('/request') && sourcePage === COMMON_STREAM_URL.STATEMENT_TURNOVER:
       return METRIC_ACTION.HIDDEN_VIEW_STATEMENT_REQUEST;
+    case url.endsWith('/request'):
+      return METRIC_ACTION.STATEMENT_REQUEST;
     case url.endsWith('/create-attachment') ||
+      exportAttachment.test(url) ||
       exportStatementRegEx.test(url) ||
-      printStatementRegEx.test(url) ||
-      exportAttachment.test(url):
+      printStatementRegEx.test(url):
       return METRIC_ACTION.DOWNLOAD_ATTACHMENT;
     case url.endsWith('/request/get-page'):
       return METRIC_ACTION.STATEMENT_REQUEST_GET_PAGE;
