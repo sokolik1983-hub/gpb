f27945d5ee51cb73f25eaa27e4854b2cc9526bae
Ivan Lyalyakin <ivan.lyalyakin@cosysoft.ru>
2022-09-19 17:57:35 +0400

    feat: GBO-33318 Реализация действия просмотра журнала проводок из запроса

--- src/actions/admin/index.ts
+++ src/actions/admin/index.ts
@@ -1,2 +1,3 @@
 export * from './executor';
 export * from './show-statement-request-form';
+export * from './show-transaction-scroller';
---
+++ src/actions/admin/show-transaction-scroller.ts
@@ -0,0 +1,15 @@
+import type { context } from 'actions/admin';
+import type { RequestTransactions } from 'interfaces/admin';
+import { ADMIN_STREAM_URL } from 'stream-constants/admin';
+import type { IActionConfig } from '@platform/services';
+
+/** Показать журнал проводок из запроса. */
+export const showTransactionsScroller: IActionConfig<typeof context, Promise<void>> = {
+  action: ({ done }, { router }) => async ({ id, refererPage }: RequestTransactions) => {
+    router.push(`${ADMIN_STREAM_URL.TRANSACTION_REQUESTS}/${id}`, { refererPage });
+
+    done();
+
+    return Promise.resolve();
+  },
+};
