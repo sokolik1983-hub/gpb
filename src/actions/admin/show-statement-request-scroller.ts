aca9ec1e98a40139175d40ebfbd47cfc8fc3cdfe
Ivan Lyalyakin <ivan.lyalyakin@cosysoft.ru>
2022-09-20 10:27:28 +0400

    feat: GBO-33399 Реализация действия просмотра журнала запросов выписки

---
+++ src/actions/admin/show-statement-request-scroller.ts
@@ -0,0 +1,15 @@
+import type { context } from 'actions/admin';
+import type { StatementRequest } from 'interfaces/admin';
+import { ADMIN_STREAM_URL } from 'stream-constants/admin';
+import type { IActionConfig } from '@platform/services';
+
+/** Показать журнал запросов выписки. */
+export const showStatementRequestScroller: IActionConfig<typeof context, Promise<void>> = {
+  action: ({ done }, { router }) => async ({ refererPage }: StatementRequest) => {
+    router.push(`${ADMIN_STREAM_URL.STATEMENT_REQUESTS}`, { refererPage });
+
+    done();
+
+    return Promise.resolve();
+  },
+};
--- src/interfaces/admin/index.ts
+++ src/interfaces/admin/index.ts
@@ -1,2 +1,3 @@
 export * from './statement-request';
+export * from './statement-request-scroller';
 export * from './request-transactions';
