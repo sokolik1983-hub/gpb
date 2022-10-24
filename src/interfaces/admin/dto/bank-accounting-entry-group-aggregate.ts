ca5f39ba52452ad806bd44962400abe5b928f912
Vladimir Mitrokhin <v.mitrokhin@bssys.com>
2022-10-24 14:22:58 +0300

    feat(скроллер): GBO-26256 обновлено дто ответа, содержащего результаты операций по проводкам

--- src/interfaces/admin/dto/bank-accounting-entry-group-aggregate.ts
+++ src/interfaces/admin/dto/bank-accounting-entry-group-aggregate.ts
@@ -10,8 +10,12 @@ export interface BankAccountingEntryGroupAggregate {
   amountDebit: number;
   /** Входящий остаток. */
   incomingBalance: number;
+  /** Количество поступлений. */
+  incomingCount: number;
   /** Исходящий остаток. */
   outgoingBalance: number;
+  /** Количество списаний. */
+  outgoingCount: number;
   /** Общее количество. */
   totalCount: number;
   /** Обороты по кредиту. */
