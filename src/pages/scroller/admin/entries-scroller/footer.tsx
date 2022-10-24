2e2c0975a86911114be1276b524a16e1965d8175
Ivan Lyalyakin <ivan.lyalyakin@cosysoft.ru>
2022-10-24 15:02:00 +0400

    feat: GBO-36107 Добавить тултип в футер скроллера проводок

--- src/pages/scroller/admin/entries-scroller/footer.tsx
+++ src/pages/scroller/admin/entries-scroller/footer.tsx
@@ -1,11 +1,7 @@
-import React, { useMemo } from 'react';
-import type { IUrlParams } from 'interfaces';
+import React from 'react';
 import type { BankAccountingEntryCard } from 'interfaces/admin/dto/bank-accounting-entry-card';
-import type { BankAccountingEntryGroupAggregate } from 'interfaces/admin/dto/bank-accounting-entry-group-aggregate';
 import { locale } from 'localization';
-import { useQueryClient } from 'react-query';
-import { useParams } from 'react-router-dom';
-import { bigNumber, formatMoney } from '@platform/tools/big-number';
+import { AccountFieldsWithTooltipPanel } from 'pages/scroller/admin/entries-scroller/components/footer-content/account-fields-with-tooltip-panel';
 import { Typography, Box, Horizon, Gap } from '@platform/ui';
 import css from './styles.scss';
 
@@ -14,51 +10,23 @@ interface FooterProps {
   /** Набор выбранных проводок. */
   selectedRows: BankAccountingEntryCard[];
 }
-
-/** Вычисляет суммарный входящий остаток по заданным ст строкам. */
-const getIncomeTotal = (rows: BankAccountingEntryCard[]) =>
-  rows.reduce((acc, item) => acc.plus(item?.incomingBalance ?? 0), bigNumber(0)).toString();
-
-/** Вычисляет суммарный исходящий остаток по заданным ст строкам. */
-const getOutcomeTotal = (rows: BankAccountingEntryCard[]) =>
-  rows.reduce((acc, item) => acc.plus(item?.outgoingBalance ?? 0), bigNumber(0)).toString();
-
 /** Компонент для вывода футера скроллера проводок. */
-export const Footer: React.FC<FooterProps> = ({ selectedRows }) => {
-  const { id } = useParams<IUrlParams>();
-  const queryClient = useQueryClient();
-
-  const total = queryClient.getQueryData<BankAccountingEntryGroupAggregate[]>(['@eco/statement', 'turnoverTotalByAccount', id]);
-  const currencyLetterCode = total?.[0]?.account?.currency?.letterCode ?? '';
-
-  // суммарный входящий остаток по выбранным проводкам
-  const incomeTotal = useMemo(() => getIncomeTotal(selectedRows), [selectedRows]);
-
-  // суммарный исходящий остаток по выбранным проводкам
-  const outcomeTotal = useMemo(() => getOutcomeTotal(selectedRows), [selectedRows]);
-
-  return (
-    <Box className={css.footer} fill="BASE">
-      <Horizon>
-        <Typography.P>{locale.admin.entryScroller.footer.selected}</Typography.P>
-        <Gap.XS />
-        <Typography.PBold>{selectedRows.length}</Typography.PBold>
-        <Gap.XL />
-        <Typography.P> {locale.admin.entryScroller.footer.income}</Typography.P>
-        <Gap.XS />
-        <Typography.PBold fill="ACCENT">{formatMoney(incomeTotal)}</Typography.PBold>
-        <Gap.XS />
-        <Typography.PBold fill="ACCENT">{currencyLetterCode}</Typography.PBold>
-        <Gap.XL />
-        <Typography.P> {locale.admin.entryScroller.footer.outcome}</Typography.P>
-        <Gap.XS />
-        <Typography.PBold fill="ACCENT">{formatMoney(outcomeTotal)}</Typography.PBold>
-        <Gap.XS />
-        <Typography.PBold fill="ACCENT">{currencyLetterCode}</Typography.PBold>
-        <Gap.XS />
-      </Horizon>
-    </Box>
-  );
-};
+export const Footer: React.FC<FooterProps> = ({ selectedRows }) => (
+  <Box className={css.footer} fill="BASE">
+    <Horizon>
+      <Typography.P>{locale.admin.entryScroller.footer.selected}</Typography.P>
+      <Gap.XS />
+      <Typography.PBold>{selectedRows.length}</Typography.PBold>
+      <Gap.XL />
+      <Typography.P> {locale.admin.entryScroller.footer.outcome}</Typography.P>
+      <Gap.XS />
+      <AccountFieldsWithTooltipPanel isDebit={false} payments={selectedRows} />
+      <Gap.XL />
+      <Typography.P> {locale.admin.entryScroller.footer.income}</Typography.P>
+      <Gap.XS />
+      <AccountFieldsWithTooltipPanel isDebit payments={selectedRows} />
+    </Horizon>
+  </Box>
+);
 
 Footer.displayName = 'Footer';
