ae8f09eacb6c0cec7dc0fdd9f7af1bc24050d013
Ivan Lyalyakin <ivan.lyalyakin@cosysoft.ru>
2022-11-25 13:19:23 +0400

    fix: GBO-38624 Исправлен запрос реестра выписок

--- src/services/admin/statement-service.ts
+++ src/services/admin/statement-service.ts
@@ -178,7 +178,7 @@ export const statementService = {
   }): Promise<IFileDataResponse> =>
     request<IServerDataResp<IFileDataResponse>>({
       data: {
-        statementRequestIds,
+        ids: statementRequestIds,
         dateFrom,
         dateTo,
         format,
