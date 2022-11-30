21382286f8b9e5558bfa6e5f530a8a5e0a911d85
Ivan Lyalyakin <ivan.lyalyakin@cosysoft.ru>
2022-11-30 11:52:59 +0400

    fix: GBO-38142 Актуализация DTO остатков счёта

--- src/interfaces/admin/dto/bank-accounting-entry-group-aggregate.ts
+++ src/interfaces/admin/dto/bank-accounting-entry-group-aggregate.ts
@@ -8,18 +8,10 @@ export interface BankAccountingEntryGroupAggregate {
   amountCredit: number;
   /** Сумма по дебету. */
   amountDebit: number;
-  /** Входящий остаток. */
-  incomingBalance: number;
   /** Количество поступлений. */
   incomingCount: number;
-  /** Исходящий остаток. */
-  outgoingBalance: number;
   /** Количество списаний. */
   outgoingCount: number;
   /** Общее количество. */
   totalCount: number;
-  /** Обороты по кредиту. */
-  turnoverCredit: number;
-  /** Обороты по дебету. */
-  turnoverDebit: number;
 }
